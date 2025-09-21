db0f5e47e045c58f244b930f017d4784801f94d5
Ethan Shanahan <97416981+ethan-shanahan@users.noreply.github.com>
2025-09-21 18:28:13 +0200

    add slugify function to utils.py

--- src/pokemanager/utils.py
+++ src/pokemanager/utils.py
@@ -1,8 +1,7 @@
-"""Utility functions for pokemanager.
+"""Utility functions for pokemanager."""
 
-This module provides helper functions such as combinations for list processing.
-"""
-
+import re
+import unicodedata
 from typing import Any
 
 
@@ -16,8 +15,25 @@ def combinations(collection: list[Any], combination_length: int) -> list[list[An
         return [[element] for element in collection]
     result: list[list[Any]] = []
     for i in range(len(collection) - combination_length + 1):
-        head: list[Any] = collection[i:i+1]
-        tail: list[list[Any]] = combinations(collection[i+1:], combination_length - 1)
+        head: list[Any] = collection[i : i + 1]
+        tail: list[list[Any]] = combinations(collection[i + 1 :], combination_length - 1)
         for t in tail:
             result.append(head + t)
     return result
+
+
+def slugify(value: Any, allow_unicode: bool = False):
+    """Slugify a string.
+
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize("NFKC", value)
+    else:
+        value = unicodedata.normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
